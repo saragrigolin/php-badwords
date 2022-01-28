da278bdc677743bf01f2dfea8d4e29aca1ba552c
saragrigolin <sara.grigolin95@gmail.com>
2022-01-28 14:53:50 +0100

    add case insensitive

--- index.php
+++ index.php
@@ -5,9 +5,14 @@ Una parola da censurare viene passata dall’utente tramite parametro GET.
 Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. -->
 
 <?php
+//dichiaro il paragrafo
 $paragraph = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, quisquam nostrum. Consequuntur dicta, sint qui dolore quisquam, perspiciatis eaque illo incidunt aliquam harum facere ducimus dolorem aliquid ad officia quae.";
-$censura = $_GET["censura"];
-$paragraph2 = str_replace($censura, "***", $paragraph);
+
+//dichiaro la variabile della query
+$censured = $_GET["censured"];
+
+//sostituisco la parola censurata
+$paragraph2 = str_ireplace($censured, "***", $paragraph);
 ?>
 
 <!DOCTYPE html>
@@ -21,7 +26,7 @@ $paragraph2 = str_replace($censura, "***", $paragraph);
 <body>
     <p>Frase iniziale: "<?php echo $paragraph; ?>"</p>
     <p>Lunghezza stringa: <?php echo strlen($paragraph); ?></p>
-    <p>Aggiungi all'URL la seguente stringa ?censura="parola" dove la parola è quello che vuoi censurare.</p>
+    <p>Aggiungi all'URL la seguente stringa ?censured="parola" dove la parola è quello che vuoi censurare.</p>
     <p>Frase censurata: "<?php echo $paragraph2; ?>"</p>
     <p>Lunghezza stringa: <?php echo strlen($paragraph2); ?></p>
 </body>
